8ed3f1aec5dc9fb93e6e0c0f4363037cbf5a1348
Kalaiarasankasi <kalaikasi166@gmail.com>
2024-01-31 19:31:47 +0530

    while loop program

---
+++ while else.py
@@ -0,0 +1,15 @@
+# While else & For else
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+else:
+    print(" loop completed")
+
+for i in range(1,21):
+    if i == 5:
+       break
+    print(i)
+else:
+    ("For loop completed")
